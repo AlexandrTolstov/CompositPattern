e47ace00805ff8f2fa7a28625cf4e442e1907073
Александр Толстов <@local.itstep>
2019-03-23 09:23:21 +0600

    Add project files.

---
+++ CompositPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
